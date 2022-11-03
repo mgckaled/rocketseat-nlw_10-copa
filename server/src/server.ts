43b7f989fe8c68a5825232dbfc5f08a9515f31c1
mgckaled <marcelgckaled@gmail.com>
2022-11-02 23:25:39 -0300

    stage 2/5 server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import FastiFy from 'fastify'
 import cors from '@fastify/cors'
+import { z } from 'zod'
 import { PrismaClient } from '@prisma/client'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
 	// Log de todas as querys no terminal para debug
@@ -17,11 +19,42 @@ async function bootstrap() {
 		origin: 'true' // permite qualquer aplicação acessar o backend
 	})
 
-	// Rota -> http://localhost:3333/pools/count
 	fastify.get('/pools/count', async () => {
 		const count = await prisma.pool.count()
 
 		return { count }
+	})
+
+	fastify.get('/users/count', async () => {
+		const count = await prisma.user.count()
+
+		return { count }
+	})
+
+	fastify.get('/guesses/count', async () => {
+		const count = await prisma.guess.count()
+
+		return { count }
+	})
+
+	fastify.post('/pools', async (request, reply) => {
+		const createPoolBody = z.object({
+			title: z.string()
+		})
+
+		const { title } = createPoolBody.parse(request.body)
+
+		const generate = new ShortUniqueId({ length: 6 })
+		const code = String(generate()).toUpperCase()
+
+		await prisma.pool.create({
+			data: {
+				title,
+				code
+			}
+		})
+
+		return reply.status(201).send({ code })
 	})
 
 	// raiz do servidor -> http://localhost:3333
