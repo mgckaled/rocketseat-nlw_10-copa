8c7afdbe0c7bc4148b10abc2be076d1ec0ee9d7a
mgckaled <marcelgckaled@gmail.com>
2022-11-03 11:46:41 -0300

    stage 1/5 mobile done

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from 'native-base'
+
+export function SignIn() {
+	return (
+		<Center flex={1} bgColor="gray.900">
+			<Text color="white" fontSize="28" fontFamily="heading">
+				SignIn
+			</Text>
+		</Center>
+	)
+}
