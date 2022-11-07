c393772baa5a591c5f5fd7e2e664b19aa1d53e45
mgckaled <marcelgckaled@gmail.com>
2022-11-07 17:27:30 -0300

    feat:[mobile] - stage 4/5 backend integration

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactNode, useState, useEffect } from 'react'
 import * as Google from 'expo-auth-session/providers/google'
 import * as AuthSession from 'expo-auth-session'
 import * as WebBrowser from 'expo-web-browser'
+import { api } from '../services/api'
 
 // garantir o redirecionamento do navegador
 WebBrowser.maybeCompleteAuthSession()
@@ -51,7 +52,23 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
 
 	async function sighInWithGoogle(access_token: string) {
 		// retorna a chave de autenticação que irá buscar os dados do usuário
-		console.log('TOKEN DE AUTENTICAÇÃO => ', access_token)
+		// console.log('TOKEN DE AUTENTICAÇÃO => ', access_token)
+
+		try {
+			setIsUserLoading(true)
+			const tokenResponse = await api.post('/users', { access_token })
+			api.defaults.headers.common[
+				'Authorization'
+			] = `Bearer ${tokenResponse.data.token}`
+
+			const userInfoResponse = await api.get('/me')
+			setUser(userInfoResponse.data.user)
+		} catch (error) {
+			console.log(error)
+			throw error
+		} finally {
+			setIsUserLoading(false)
+		}
 	}
 
 	useEffect(() => {
